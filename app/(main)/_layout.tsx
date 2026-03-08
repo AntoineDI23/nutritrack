915b5dfed248f392626912da228a7196d46fffd3
AntoineDI23 <antoine.simeon@viacesi.fr>
2026-03-08 23:44:13 +0100

    [Feat] Add icons for "Home", "Add" and "Profile"

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { ActivityIndicator, StyleSheet } from "react-native";
 import { Redirect, Tabs } from "expo-router";
 import { useAuth } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
 import { ThemedView } from "@/components/themed-view";
 
 export default function MainLayout() {
@@ -25,18 +26,27 @@ export default function MainLayout() {
         name="(home)"
         options={{
           title: "Accueil",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="add"
         options={{
           title: "Ajouter",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="add-circle" size={size} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: "Profil",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" size={size} color={color} />
+          ),
         }}
       />
     </Tabs>
